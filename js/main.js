d85df082ed40d4b527f6656b671fa1d50409c70d
justin <justinobon07@gmail.com>
2025-11-10 21:47:11 +0000

    Premier commit

--- js/main.js
+++ js/main.js
@@ -115,3 +115,20 @@
 	});
 
 })(jQuery);
+
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+	  loop:true,
+	  margin:10,
+	  nav:true,
+	  dots:true,
+	  autoplay:true,
+	  autoplayTimeout:5000,
+	  responsive:{
+		0:{ items:1 },
+		600:{ items:2 },
+		1000:{ items:3 }
+	  }
+	});
+  });
+  
